7eb875478f13953224aada07eb2abda575ddf7e2
rblood79 <rblood79@gmail.com>
2025-02-19 10:52:37 +0900

    firebase.js에서 주석 제거 및 코드 정리

--- src/firebase.js
+++ src/firebase.js
@@ -12,16 +12,3 @@ const firebaseConfig = {
 };
 
 export default firebase.initializeApp(firebaseConfig);
-
-
-/*const firebaseConfig = {
-    apiKey: "AIzaSyD4lFYlGsKryHvQUF5CT3wUdhnU_uQBVUA",
-    authDomain: "army-c3fb4.firebaseapp.com",
-    projectId: "army-c3fb4",
-    storageBucket: "army-c3fb4.appspot.com",
-    messagingSenderId: "806478232160",
-    appId: "1:806478232160:web:893774803a8773caa12105",
-    measurementId: "G-SX7YFMRCJZ"
-}
-
-export default firebase.initializeApp(firebaseConfig);*/
